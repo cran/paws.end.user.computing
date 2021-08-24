4cd19229a923589419e3087a4851d04aa4830756
David Kretch <david.kretch@gmail.com>
2021-08-24 18:30:02 +0000

    version 0.1.12

---
+++ tests/testthat/test_worklink.R
@@ -0,0 +1,9 @@
+svc <- paws::worklink()
+
+test_that("list_fleets", {
+  expect_error(svc$list_fleets(), NA)
+})
+
+test_that("list_fleets", {
+  expect_error(svc$list_fleets(MaxResults = 20), NA)
+})
