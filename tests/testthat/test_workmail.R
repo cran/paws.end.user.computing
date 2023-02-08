2276265fc9d9dbc8d2afd3f41a4e0f08400166c3
Dyfan Jones <dyfan.r.jones@gmail.com>
2023-02-08 11:40:02 +0000

    version 0.2.0

---
+++ tests/testthat/test_workmail.R
@@ -0,0 +1,9 @@
+svc <- paws::workmail()
+
+test_that("list_organizations", {
+  expect_error(svc$list_organizations(), NA)
+})
+
+test_that("list_organizations", {
+  expect_error(svc$list_organizations(MaxResults = 20), NA)
+})
